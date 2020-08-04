512479e29e28e2e28b4203cdee99e8754a10bb11
Kevin C <kevincolejr@outlook.com>
2020-08-04 18:16:06 -0400

    added GET request. Got response from API endpoint

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import requests
+import json
 
+response = requests.get("http://api.open-notify.org/iss-now.json")
+json_response = response.json()
+longitude = json_response["iss_position"]["longitude"]
+latitude = json_response["iss_position"]["latitude"]
+
+print(latitude,longitude)
