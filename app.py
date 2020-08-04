1722ae35197ee7ea709151f951931499ce1a76b0
Kevin C <kevincolejr@outlook.com>
2020-08-04 18:34:41 -0400

    added commentst

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import requests
-import json
+import requests #import request library the standard for making http request with Python
+import json # Import library used to work with JSON
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
-json_response = response.json()
-longitude = json_response["iss_position"]["longitude"]
-latitude = json_response["iss_position"]["latitude"]
+response = requests.get("http://api.open-notify.org/iss-now.json") #send GET request to API enpoint
+json_response = response.json() #store the json response in a variable named json_reponse
+longitude = json_response["iss_position"]["longitude"] #extract the longitude from the JSON response object
+latitude = json_response["iss_position"]["latitude"] #extract the latitide from the JSON repsonse object
 
-print(latitude,longitude)
+print(latitude,longitude) #print to the console the latitide and longitude of the international space station
